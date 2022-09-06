ae67b836fc0bd5a6d41e0b0e575f5b5488412238
Kuthan Temimhan <kuthan35@gmail.com>
2022-09-06 22:51:31 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcResume.Models.Entity;
+
+
+namespace MvcResume.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        ResumeDbEntities db = new ResumeDbEntities();
+        public ActionResult Index()
+        {
+            var AboutList = db.TblAbouts.ToList();
+            return View(AboutList);
+        }
+    }
+}
